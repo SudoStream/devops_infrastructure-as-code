8a8d515622628ac882ea6105aa8c4775e58d7393
Andy Boyle <andy@sudostream.io>
2017-01-20 15:09:23 +0000

    add mkdir for terraform state location

--- terraform/iacApply.sh
+++ terraform/iacApply.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-#gcloud docker login -e 1234@5678.com -u _json_key -p "$(cat /home/andy/.secrets/api-event-horizon-33d2211db244.json)" https://gcr.io
+mkdir -p ${HOME}/iac
+
 terraform apply -state=${HOME}/iac/terraform.tfstate
 
