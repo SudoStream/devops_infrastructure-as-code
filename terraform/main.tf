5b585fbe0597916680f871b6738678716f32674b
Andy Boyle <andy@sudostream.io>
2017-01-23 13:56:43 +0000

    refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,16 +8,10 @@ resource "google_compute_instance" "kafka-server" {
   name         = "kafka-server-1"
   machine_type = "n1-standard-1"
   zone         = "europe-west1-b"
-
   tags = ["kafka", "messaging"]
 
   disk {
-    //image = "ubuntu-os-cloud/ubuntu-1604-lts"
     image = "aeh-kafka-1484686751"
-  }
-
-  // Local SSD disk
-  disk {
     type    = "local-ssd"
     scratch = true
   }
@@ -29,27 +23,40 @@ resource "google_compute_instance" "kafka-server" {
     }
   }
 
-  metadata {
-    foo = "bar"
-  }
-
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 }
 
+resource "google_compute_address" "kafka-server-address" {
+    name = "kafka-server-address"
+}
+
+resource "google_compute_forwarding_rule" "http" {
+  name = "tf-www-http-forwarding-rule"
+  target = "${google_compute_instance.kafka-server.self_link}"
+  ip_address = "${google_compute_address.kafka-server-address.address}"
+  port_range = "80"
+}
+
+resource "google_compute_forwarding_rule" "https" {
+  name = "tf-www-https-forwarding-rule"
+  target = "${google_compute_instance.kafka-server.self_link}"
+  ip_address = "${google_compute_address.kafka-server-address.address}"
+  port_range = "443"
+}
+
 resource "google_dns_managed_zone" "dev" {
   name     = "dev-zone"
   dns_name = "dev.sudostream.io."
+  description = "Sudostream Dev"
 }
 
 resource "google_dns_record_set" "kafka" {
   name = "kafka.${google_dns_managed_zone.dev.dns_name}"
   type = "A"
   ttl  = 300
-
   managed_zone = "${google_dns_managed_zone.dev.name}"
-
   rrdatas = ["${google_compute_instance.kafka-server.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
 
