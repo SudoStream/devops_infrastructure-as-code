ae763e19fba4c53d07466ceb6cd8058a480e92a1
Andy Boyle <andy@sudostream.io>
2017-11-10 15:01:01 +0000

    correct project id

--- terraform/enduring/main.tf
+++ terraform/enduring/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   credentials = "${file("/home/andy/.secrets/time-to-teach-zone-ffc79db65fb1.json")}"
-  project     = "time-to-teach"
+  project     = "time-to-teach-zone"
   region      = "europe-west1"
 }
 
