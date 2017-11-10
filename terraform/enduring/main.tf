7f0a4996f1fc75aec8654b53a0319a80204d0cbb
Andy Boyle <andy@sudostream.io>
2017-11-10 14:56:03 +0000

    update local ttt json file again2

--- terraform/enduring/main.tf
+++ terraform/enduring/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("/home/andy/.secrets/time-to-teach-zone-a414655775c4.json")}"
+  credentials = "${file("/home/andy/.secrets/time-to-teach-zone-ffc79db65fb1.json")}"
   project     = "time-to-teach"
   region      = "europe-west1"
 }
